56a5f27ba8d4b2766ee9772747b52e8bd688fb29
Jeonghun Ha <maemenaver@gmail.com>
2022-08-04 22:07:09 +0900

    ✨ Implement moon

--- src/canvas/Main.tsx
+++ src/canvas/Main.tsx
@@ -4,6 +4,7 @@ import { OrbitControls, PerspectiveCamera } from "@react-three/drei";
 import { useThree } from "@react-three/fiber";
 import { Earth } from "./model/Earth";
 import { Sun } from "./model/Sun";
+import { Moon } from "./model/Moon";
 
 export const Main = () => {
     const scene = useThree((state) => state.scene);
@@ -23,6 +24,9 @@ export const Main = () => {
                 <Sun />
                 <group>
                     <Earth position={[-2000, 0, 0]} scale={0.25} />
+                    <group>
+                        <Moon position={[-1500, 0, 0]} scale={0.1} />
+                    </group>
                 </group>
             </group>
             <OrbitControls />
--- src/canvas/model/Moon.tsx
+++ src/canvas/model/Moon.tsx
@@ -1,3 +1,37 @@
-export const Moon = () => {
-    return <></>;
-};
+import * as THREE from "three";
+import { useTexture } from "@react-three/drei";
+import React, { useEffect, useRef } from "react";
+import { DRACOLoader } from "three/examples/jsm/loaders/DRACOLoader";
+import { GroupProps } from "@react-three/fiber";
+
+export const Moon = React.memo((props?: GroupProps) => {
+    const moonRef = useRef<THREE.Group>();
+
+    const diffuse = useTexture("/model/moon/diffuse.png");
+
+    useEffect(() => {
+        const func = async () => {
+            const dracoLoader = new DRACOLoader();
+            dracoLoader.setDecoderPath("/draco/");
+            dracoLoader.preload();
+            const geometry = await dracoLoader.loadAsync(
+                "/model/moon/geometry.drc"
+            );
+            const material = new THREE.MeshStandardMaterial();
+            material.map = diffuse;
+            material.side = THREE.DoubleSide;
+
+            const mesh = new THREE.Mesh(geometry, material);
+            moonRef.current.add(mesh);
+
+            console.log(moonRef.current);
+        };
+        func();
+    }, []);
+
+    return (
+        <>
+            <group ref={moonRef} name="moon" {...props}></group>
+        </>
+    );
+});
