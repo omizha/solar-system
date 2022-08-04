010dff126ab5c64cb122f9d90675e4ba05e6b2ec
Jeonghun Ha <maemenaver@gmail.com>
2022-08-04 22:17:52 +0900

    💄 Add earth normalMap

--- src/canvas/model/Earth.tsx
+++ src/canvas/model/Earth.tsx
@@ -7,7 +7,10 @@ import { GroupProps } from "@react-three/fiber";
 export const Earth = React.memo((props?: GroupProps) => {
     const earthRef = useRef<THREE.Group>();
 
-    const diffuse = useTexture("/model/earth/diffuse.png");
+    const [diffuse, normal] = useTexture([
+        "/model/earth/diffuse.png",
+        "/model/earth/normal.png",
+    ]);
 
     useEffect(() => {
         const func = async () => {
@@ -19,6 +22,8 @@ export const Earth = React.memo((props?: GroupProps) => {
             );
             const material = new THREE.MeshStandardMaterial();
             material.map = diffuse;
+            // 노말맵이 적용된 것인가? (육안으로는 확인이 어렵지만, console.log에는 텍스처가 적용돼있다는 것을 알 수 있다.)
+            material.normalMap = normal;
             material.side = THREE.DoubleSide;
 
             const mesh = new THREE.Mesh(geometry, material);
