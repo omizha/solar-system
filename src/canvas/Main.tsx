91037ce6a4f55d91d443fe08c8785241f6c87f1f
Jeonghun Ha <maemenaver@gmail.com>
2022-08-04 22:05:03 +0900

    ✨ Implement earth

--- src/canvas/Main.tsx
+++ src/canvas/Main.tsx
@@ -1,14 +1,12 @@
-import { OrbitControls, PerspectiveCamera } from "@react-three/drei";
-import { useLoader, useThree } from "@react-three/fiber";
 import { useEffect } from "react";
 import * as THREE from "three";
-import { DRACOLoader } from "three/examples/jsm/loaders/DRACOLoader";
+import { OrbitControls, PerspectiveCamera } from "@react-three/drei";
+import { useThree } from "@react-three/fiber";
+import { Earth } from "./model/Earth";
 import { Sun } from "./model/Sun";
 
 export const Main = () => {
     const scene = useThree((state) => state.scene);
-
-    // useLoader(DRACOLoader, input);
 
     useEffect(() => {
         scene.background = new THREE.Color("#000");
@@ -16,8 +14,17 @@ export const Main = () => {
 
     return (
         <>
-            <PerspectiveCamera makeDefault position={[0, 0, -2000]} />
-            <Sun />
+            <PerspectiveCamera
+                makeDefault
+                position={[0, 0, -3000]}
+                far={10000}
+            />
+            <group>
+                <Sun />
+                <group>
+                    <Earth position={[-2000, 0, 0]} scale={0.25} />
+                </group>
+            </group>
             <OrbitControls />
             <ambientLight />
         </>
--- src/canvas/model/Earth.tsx
+++ src/canvas/model/Earth.tsx
@@ -1,3 +1,37 @@
-export const Earth = () => {
-    return <></>;
-};
+import * as THREE from "three";
+import { useTexture } from "@react-three/drei";
+import React, { useEffect, useRef } from "react";
+import { DRACOLoader } from "three/examples/jsm/loaders/DRACOLoader";
+import { GroupProps } from "@react-three/fiber";
+
+export const Earth = React.memo((props?: GroupProps) => {
+    const earthRef = useRef<THREE.Group>();
+
+    const diffuse = useTexture("/model/earth/diffuse.png");
+
+    useEffect(() => {
+        const func = async () => {
+            const dracoLoader = new DRACOLoader();
+            dracoLoader.setDecoderPath("/draco/");
+            dracoLoader.preload();
+            const geometry = await dracoLoader.loadAsync(
+                "/model/earth/geometry.drc"
+            );
+            const material = new THREE.MeshStandardMaterial();
+            material.map = diffuse;
+            material.side = THREE.DoubleSide;
+
+            const mesh = new THREE.Mesh(geometry, material);
+            earthRef.current.add(mesh);
+
+            console.log(earthRef.current);
+        };
+        func();
+    }, []);
+
+    return (
+        <>
+            <group ref={earthRef} name="earth" {...props}></group>
+        </>
+    );
+});
