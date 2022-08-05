8f1c4d2af988a3764e41af7769223984f46a05e8
Jeonghun Ha <maemenaver@gmail.com>
2022-08-06 00:33:22 +0900

    ✨ Implement solar system orbit

--- src/canvas/Main.tsx
+++ src/canvas/Main.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 import * as THREE from "three";
 import { OrbitControls, PerspectiveCamera } from "@react-three/drei";
 import { useFrame, useThree } from "@react-three/fiber";
@@ -7,22 +7,30 @@ import { Sun } from "./model/Sun";
 import { Moon } from "./model/Moon";
 
 export const Main = () => {
+    const sunOrbitRef = useRef<THREE.Group>();
+    const earthOrbitRef = useRef<THREE.Group>();
+
     const scene = useThree((state) => state.scene);
 
     useEffect(() => {
         scene.background = new THREE.Color("#000");
     }, []);
 
+    useFrame((_, delta) => {
+        sunOrbitRef.current.rotation.y += (delta * Math.PI * 2) / 365;
+        earthOrbitRef.current.rotation.y += (delta * Math.PI * 2) / 27;
+    });
+
     return (
         <>
             <PerspectiveCamera
                 makeDefault
-                position={[0, 0, -3000]}
+                position={[0, 4000, -1000]}
                 far={10000}
             />
-            <group>
+            <group ref={sunOrbitRef}>
                 <Sun />
-                <group position={[-2000, 0, 0]}>
+                <group ref={earthOrbitRef} position={[-2000, 0, 0]}>
                     <Earth scale={0.25} />
                     <group position={[500, 0, 0]}>
                         <Moon scale={0.1} />
